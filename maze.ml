b2f5f58dc1c4a02e672492ec367460090b249364
yoshihiro503 <yimai.v@gmail.com>
2010-02-08 17:56:12 +0900

    asfd

--- maze.ml
+++ maze.ml
@@ -1,29 +1,27 @@
 open Util
 
-type node = string
-type t = { id: string; pass: string; start: node; goal: node}
-
 let parse_input () =
-  prerr_string "target: @";
-  let gl = read_line() in
-  prerr_string "your id: @";
-  let twid = read_line() in
-  prerr_string "password: ";
-  let ps = read_line() in
-  { id=twid; pass=ps; start=twid; goal=gl}
-    
-let snode node = "@"^node
-let m = parse_input ()
+  let rec iter store =
+    try
+      iter (explode (read_line()) :: store)
+    with
+    | End_of_file -> List.rev store
+  in
+  matrix_of_list @@ iter []
 
-(*
+type t = char array array
+type node = int * int
+
+let snode (i,j) = "("^si i^","^si j^")"
+let m = parse_input ()
 let get : t -> node -> char
     = fun m (i,j) -> m.(j).(i)
 let set : t -> node -> char -> unit
     = fun m (i,j) c -> m.(j).(i) <- c
 let sidx (i,j) =
   si i ^ "," ^ si j
-*)
-(*let width, height = (Array.length m.(0), Array.length m)*)
+
+let width, height = (Array.length m.(0), Array.length m)
     
 let start : node
     = matrix_find_idx ((=) 'S') m
