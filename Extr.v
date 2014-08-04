06a6424a06c22ae78e2b23a9766c444aed6e27f8
yoshihiro503 <y.imai@ocaml.jp>
2014-08-04 20:52:34 +0900

    Extractの時はExtrOcamlBasicの基本モジュールを使うように。

--- Extr.v
+++ Extr.v
@@ -1,29 +1,5 @@
-Require Import Util.
 Require Import Solver.
-Require Import List.
-
-(* unit *)
-Extract Inductive unit => "unit" ["()"].
-
-(* pair *)
-Extract Inductive prod => "(*)" [ "(,)" ].
-Extract Constant fst => "fst".
-Extract Constant snd => "snd".
-
-(* bool *)
-Extract Inductive bool => "bool" ["true" "false"].
-
-(* sumbool *)
-Extract Inductive sumbool => "bool" ["true" "false"].
-
-(* list *)
-Extract Inductive list => "list" ["[]" "(::)"].
-Extract Constant map => "List.map".
-Extract Constant flat_map => "(fun f xs -> List.flatten (List.map f xs))".
-
-(* option *)
-Extract Inductive option => "option" ["Some" "None" ].
-
+Require Import ExtrOcamlBasic.
 
 Extract Constant maze => "Maze.t".
 Extract Constant node => "Maze.node".
